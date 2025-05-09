d21893c8039482521f31e8d6357307cca8866952
Tiagos121 <cm20.tiago.carmo@gmail.com>
2025-05-10 00:08:59 +0100

    sweetalert no delite generos
    foi adicionado ao projeto uma biblioteca na página lista generos

--- components/cp_lista_generos.php
+++ components/cp_lista_generos.php
@@ -7,6 +7,10 @@ require_once "./scripts/sc_error_feedback.php";
 $link = new_db_connection();
 ?>
 
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.21.0/dist/sweetalert2.all.min.js"></script>
+<link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.21.0/dist/sweetalert2.min.css" rel="stylesheet">
+
+
 
 <section class="sec-filmes pb-5" id="lista-filmes">
     <div class="container px-lg-5 pt-5"> <!-- alterado de pt-3 para pt-5 para dar espaço abaixo da navbar -->
@@ -17,6 +21,7 @@ $link = new_db_connection();
                         <p class="text-black-60 text-left pb-4">Todos os filmes deste género <br>
                             Escolhe um!</p>
                     </div>
+
 
 
             <?php
@@ -34,7 +39,7 @@ $link = new_db_connection();
                     // Só se for admin é que aparece o "editar" e o "x"
                     if (isset($_SESSION['role']) && $_SESSION['role'] == 1) {
                         echo " <a href='update_genero.php?id=$id_generos' class='text-danger' >(editar)</a>";
-                        echo " <a href='scripts/generos/sc_delete_genero.php?id=$id_generos'  class='text-dark' onclick=\"return confirm('Tens a certeza que queres apagar este género?');\">(x)</a>";
+                        echo " <a href='scripts/generos/sc_delete_genero.php?id=$id_generos' class='delite-genero text-dark'>(x)</a>";
                     }
 
                     echo "</h3>";
@@ -58,3 +63,31 @@ $link = new_db_connection();
         ?>
     </div>
 </section>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const deletes = document.querySelectorAll('.delite-genero');
+
+        deletes.forEach(link => {
+            link.addEventListener('click', function(e) {
+                e.preventDefault();
+
+                Swal.fire({
+                    title: "Tens a certeza?",
+                    text: "Tu não vais conseguir reverter isto!",
+                    icon: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#3085d6",
+                    cancelButtonColor: "#d33",
+                    cancelButtonText: "Cancelar",
+                    confirmButtonText: "Sim, apagar!"
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        // redireciona para o script de delete
+                        window.location.href = link.href;
+                    }
+                });
+            });
+        });
+    });
+</script>
